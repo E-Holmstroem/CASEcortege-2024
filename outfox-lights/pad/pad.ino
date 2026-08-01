5d0c285355adc78046a81f3b057c9cf894ddf578
E-Holmstroem <119513138+E-Holmstroem@users.noreply.github.com>
2026-08-01 16:32:58 +0200

    Stop dancepad detection for up, down, left, right
    Dancepad has been switched to commercially available so the inputs from former is no longer needed. Enter and escape buttons are kept.

--- outfox-lights/pad/pad.ino
+++ outfox-lights/pad/pad.ino
@@ -2,24 +2,24 @@
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A4, INPUT);
+  // pinMode(A1, INPUT);
+  // pinMode(A2, INPUT);
+  // pinMode(A3, INPUT);
+  // pinMode(A4, INPUT);
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   Serial.begin(115200);
 }
 
 
-int pins[] = {A1, A2, A3, A4};
+// int pins[] = {A1, A2, A3, A4};
 
-bool key[] = {false, false, false, false};
+// bool key[] = {false, false, false, false};
 
-int cooldown[] = {0, 0, 0, 0};
+// int cooldown[] = {0, 0, 0, 0};
 
-float avgs[] = {0.0f, 0.0f, 0.0f, 0.0f};
-float maxs[] = {1.0f, 1.0f, 1.0f, 1.0f};
+// float avgs[] = {0.0f, 0.0f, 0.0f, 0.0f};
+// float maxs[] = {1.0f, 1.0f, 1.0f, 1.0f};
 
 int val;
 
@@ -28,10 +28,10 @@ bool escKey = false;
 
 int n = 0;
 
-const float PRESSFORCE = 1500.0f;
-const float RELEASEFORCE = 1000.0f;
-const float PRESSCOMPENSATION = 1.2f;
-const float RELEASEFORCE_COMPENSATED = RELEASEFORCE / (PRESSFORCE * PRESSCOMPENSATION);
+// const float PRESSFORCE = 1500.0f;
+// const float RELEASEFORCE = 1000.0f;
+// const float PRESSCOMPENSATION = 1.2f;
+// const float RELEASEFORCE_COMPENSATED = RELEASEFORCE / (PRESSFORCE * PRESSCOMPENSATION);
 
 
 void loop() {
@@ -60,56 +60,56 @@ void loop() {
     escKey = !escKey;
   }
 
-  for(int i = 0; i < 4; i++){
-    val = analogRead(pins[i]);
+  // for(int i = 0; i < 4; i++){
+  //   val = analogRead(pins[i]);
 
-    avgs[i] = (avgs[i]+val-509)*0.95f;
+  //   avgs[i] = (avgs[i]+val-509)*0.95f;
 
-    #ifdef DEBUG
-    if(n <= 0 && i == 0){
-      Serial.println(avgs[i]);
-      n=50;
-    }
-    n--;
-    #endif
-    /*if(cooldown[i] > 0){
-      cooldown[i]--;
-      continue;
-    }*/
+  //   #ifdef DEBUG
+  //   if(n <= 0 && i == 0){
+  //     Serial.println(avgs[i]);
+  //     n=50;
+  //   }
+  //   n--;
+  //   #endif
+  //   /*if(cooldown[i] > 0){
+  //     cooldown[i]--;
+  //     continue;
+  //   }*/
 
-    // While a key is held, store the highest value.
-    // This is later used to scale the release sensitivity, so a harder press requires a harder release 
-    if(key[i]){
-      if(maxs[i] < avgs[i]){
-        maxs[i] = avgs[i];
-      }
-    }
+  //   // While a key is held, store the highest value.
+  //   // This is later used to scale the release sensitivity, so a harder press requires a harder release 
+  //   if(key[i]){
+  //     if(maxs[i] < avgs[i]){
+  //       maxs[i] = avgs[i];
+  //     }
+  //   }
 
-    if(avgs[i] >= 1500.0f && !key[i]){
-      maxs[i] = 1500.0f; // Reset the max level to a baseline
-      key[i] = true;
-      #ifndef DEBUG
-      Serial.write(0b10000000 + i);
-      Serial.flush();
-      #endif
-      /*
-      if(cooldown[i] > 5){
-      }else{
-        cooldown[i] ++;
-      }*/
-    }else if(key[i] && avgs[i] <= -RELEASEFORCE_COMPENSATED * maxs[i] ){
-      key[i] = false;
-      #ifndef DEBUG
-      Serial.write(0b00000000 + i);
-      Serial.flush();
-      #endif
-      /*if(cooldown[i] > 5){
-      }else{
-        cooldown[i] ++;
-      }*/
-    }
+  //   if(avgs[i] >= 1500.0f && !key[i]){
+  //     maxs[i] = 1500.0f; // Reset the max level to a baseline
+  //     key[i] = true;
+  //     #ifndef DEBUG
+  //     Serial.write(0b10000000 + i);
+  //     Serial.flush();
+  //     #endif
+  //     /*
+  //     if(cooldown[i] > 5){
+  //     }else{
+  //       cooldown[i] ++;
+  //     }*/
+  //   }else if(key[i] && avgs[i] <= -RELEASEFORCE_COMPENSATED * maxs[i] ){
+  //     key[i] = false;
+  //     #ifndef DEBUG
+  //     Serial.write(0b00000000 + i);
+  //     Serial.flush();
+  //     #endif
+  //     /*if(cooldown[i] > 5){
+  //     }else{
+  //       cooldown[i] ++;
+  //     }*/
+  //   }
     
-  }
+  // }
 
   
 }
